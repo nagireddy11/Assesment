58a1dc4443aacbbc5dc9d9d212186347f7a48b6e
nagireddy11 <99976524+nagireddy11@users.noreply.github.com>
2022-02-21 19:18:12 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,8 @@
+class run12 {
+    public static void main(String[] args) {
+        System.out.println("123");
+        int a = 10;
+        String b = "1234";
+        System.out.println("123" + a + b);
+    }
+}
